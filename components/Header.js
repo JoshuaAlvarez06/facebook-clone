45569ec63b35a124db9c371a1194924558c9c345
Joshua Alvarez <joshuaialvarez06@gmail.com>
2021-11-18 18:34:52 -0800

    Finished header portion

--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,25 @@
 import Image from 'next/image';
+import {
+  FlagIcon,
+  PlayIcon,
+  SearchIcon,
+  ShoppingCartIcon,
+} from '@heroicons/react/outline';
+import {
+  BellIcon,
+  ChatIcon,
+  ChevronDownIcon,
+  UserGroupIcon,
+  HomeIcon,
+  ViewGridIcon,
+} from '@heroicons/react/solid';
+import HeaderIcon from './HeaderIcon';
 
 const Header = () => {
   return (
-    <div>
-      <h1>header</h1>
-
+    <div className='sticky top-0 z-50 bg-white flex items-center p-2 lg:px-5 shadow-md'>
       {/* Left */}
-      <div>
+      <div className='flex items-center'>
         <Image
           src='https://cdn0.iconfinder.com/data/icons/social-messaging-ui-color-shapes-2-free/128/social-facebook-2019-circle-512.png'
           width={40}
@@ -14,14 +27,36 @@ const Header = () => {
           layout='fixed'
           alt='Facebook Logo'
         />
-        <div>
-          <input type='text' placeholder='Search Facebook' />
+        <div className='flex ml-2 items-center rounded-full bg-gray-100 p-2'>
+          <SearchIcon className='text-gray-600 h-6' />
+          <input
+            className='hidden md:inline-flex flex ml-2 items-center bg-transparent outline-none placeholder-gray-500 flex-shrink'
+            type='text'
+            placeholder='Search Facebook'
+          />
         </div>
       </div>
 
       {/* Center */}
+      <div className='flex justify-center flex-grow'>
+        <div className='flex space-x-6 md:space-x-2'>
+          <HeaderIcon active Icon={HomeIcon} />
+          <HeaderIcon Icon={FlagIcon} />
+          <HeaderIcon Icon={PlayIcon} />
+          <HeaderIcon Icon={ShoppingCartIcon} />
+          <HeaderIcon Icon={UserGroupIcon} />
+        </div>
+      </div>
 
       {/* Right */}
+      <div className='flex items-center sm:space-x-2 justify-end'>
+        {/* Profile picture */}
+        <p className='font-semibold pr-3 whitespace-nowrap'>Joshua Alvarez</p>
+        <ViewGridIcon className='icon' />
+        <ChatIcon className='icon' />
+        <BellIcon className='icon' />
+        <ChevronDownIcon className='icon' />
+      </div>
     </div>
   );
 };
---
+++ components/HeaderIcon.js
@@ -0,0 +1,13 @@
+const HeaderIcon = ({ Icon, active }) => {
+  return (
+    <div className='group cursor-pointer md:px-10 sm:h-14 md:hover:bg-gray-100 flex items-center rounded-xl active:border-b-2 active:border-blue-500'>
+      <Icon
+        className={`h-5 text-center sm:h-7 text-gray-500 mx-auto group-hover:text-blue-500 ${
+          active && 'text-blue-500'
+        }`}
+      />
+    </div>
+  );
+};
+
+export default HeaderIcon;
